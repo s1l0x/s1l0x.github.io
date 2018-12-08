6b9a71580a05b3094c310197272677e1e82388d0
s1l0x <38411949+s1l0x@users.noreply.github.com>
2018-12-07 17:59:06 -0700

    Source

---
+++ res/kovonic.js
@@ -0,0 +1,11 @@
+var ip = document.getElementById('ip');
+var toggle = document.getElementById('toggle-x6i');
+var a1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var a2 = 'onqpsrutwvyxzbadcfehgjilkmONQPSRUTWVYXZBADCFEHGJILKM';
+
+console.log("Test!");
+
+function editText(a) {
+    var op = document.getElementById('op');
+    op.value += a
+}
